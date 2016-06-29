5986cb013f45d85db30b5fcfa6a0f3118b1dacc4
Low Jeng Lam <jenglamlow@gmail.com>
2016-06-29 01:33:16 +0100

    polar coordinates

--- python/math/polar_coordinates.py
+++ python/math/polar_coordinates.py
@@ -1,1 +1,7 @@
-phase(complex(-1.0, 0.0))
+import cmath
+z = complex(input())
+print(abs(z))
+print(cmath.phase(z))
+
+# From discussion (pythonic way):
+# print(*cmath.polar(complex(input())), sep='\n')
