10c31a64b5a0f022bb02a1d9a522ece6291bf074
Low Jeng Lam <jenglamlow@gmail.com>
2016-07-09 09:48:08 +0100

    Numpy

--- python/numpy/dot_and_cross.py
+++ python/numpy/dot_and_cross.py
@@ -1,10 +1,5 @@
 import numpy
-N,M = map(int, input().split())
+N = int(input())
 A = numpy.array([input().split() for _ in range(N)],int)
 B = numpy.array([input().split() for _ in range(N)],int)
-print(A + B)
-print(A - B)
-print(A * B)
-print(A // B)
-print(A % B)
-print(A ** B)
+print(numpy.dot(A,B))
