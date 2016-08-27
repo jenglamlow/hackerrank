6da98275c17a9b0949e3414db03753cd95dad328
Low Jeng Lam <jenglamlow@gmail.com>
2016-08-27 15:09:25 +0100

    simple array sum

--- algorithm/warmup/simple_array_sum.cpp
+++ algorithm/warmup/simple_array_sum.cpp
@@ -1,8 +1,22 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
 #include <iostream>
-
+#include <algorithm>
 using namespace std;
 
-int main()
-{
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    long long int N, i, sum=0, num;
+    cin >> N;
+    for (i = 0; i < N; i++)
+    {
+        cin >> num;
+        sum += num;
+    }
+
+    cout << sum << endl;
+
     return 0;
 }
